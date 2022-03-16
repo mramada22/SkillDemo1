fbd8d9f5ddd952679ec6d65b1738135a091d66c2
mramada22 <mramada@ucsd.edu>
2022-03-16 16:53:36 -0700

    Lib + starter Code with failure

---
+++ SkillDemo1.java
@@ -0,0 +1,5 @@
+public class SkillDemo1 {
+    public static int subtract(int x, int y) {
+        return x-y;
+    }
+}
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void testSubtract() {
+        assertEquals(5, SkillDemo1.subtract(10,2));
+    }
+}
